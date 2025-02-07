e459d97b1a2643fa4ae152756bbe814590d136ed
Henrique114 <h.ferreira114@gmail.com>
2025-02-06 21:20:05 -0300

    Primeiro commit

---
+++ LicoesBeeCrowd/Algoritmo1019.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicoesBeeCrowd
+{
+    internal class Algoritmo1019 : IBaseAlgoritmo
+    {
+        public void Executar() 
+        {
+            // Transfornar a duração de tempo de segundos para o formato hh:mm:ss.
+
+            //variavel que guarda o tempo em segundos.
+            var tempoEmSegundos = int.Parse(Console.ReadLine());
+
+            //formatação do tempo em hh:mm:ss com TimeSpan
+            var tempoCompleto = TimeSpan.FromSeconds(tempoEmSegundos);
+
+            //variavwl para horas.
+            var horas = tempoCompleto.Hours;
+
+            //variavwl para minutos.
+            var minutos = tempoCompleto.Minutes;
+
+            //variavwl para segundos.
+            var segundos = tempoCompleto.Seconds;
+
+            Console.WriteLine($"{horas}:{minutos}:{segundos}");
+
+
+        }
+    }
+}
---
+++ LicoesBeeCrowd/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LicoesBeeCrowd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o numero do algoritmo que deseja executar:");
+
+            var opcaoExecucao = Console.ReadLine();
+            switch (opcaoExecucao)
+            {
+                case "2374":
+                    var algoritmo2374 = new Algoritmo2374();
+                    algoritmo2374.Executar();
+                    break;
+
+                case "1019":
+                    var algoritmo1019 = new Algoritmo1019();
+                    algoritmo1019.Executar();
+                    break;
+
+                default:
+                    Console.WriteLine("Escolha um algoritmo ja implementado. Vide Classes da solução.");
+                    break;  
+
+
+
+            }
+
+
+
+            Console.WriteLine("Finalizado!");
+            
+        }
+    }
+}
