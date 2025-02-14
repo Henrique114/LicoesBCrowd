c6cf14781124266b69cd9408f677da3eb977ffc4
Henrique114 <h.ferreira114@gmail.com>
2025-02-14 16:45:49 -0300

    Domain

---
+++ LicoesBeeCrowd.Domain/Algoritmo1930Domain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Algoritmo1930Domain
+{
+	public int CalculaTomadas(int reguas )
+    {
+		var result = reguas.Split(' ')                           // Divide a string por espaço
+            .Where(s => int.TryParse(s, out _))  // Filtra apenas os valores numéricos
+            .Select(int.Parse)                   // Converte para inteiros
+            .Sum();
+        return result - 3;
+    }
+}
